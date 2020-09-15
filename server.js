0b33f7c14add25422a8864a2c8dbd6201b1f5cc1
NoobSaiyan <ms19961998@gmail.com>
2020-09-15 20:11:07 +0530

    init middleware for users.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000
 //connecting database
 connectDB()
 
+//Init Middleware
+app.use(express.json({ extended: false }))
+
 app.get("/", (req, res) => res.send("API Running"))
 
 //Define Routes
