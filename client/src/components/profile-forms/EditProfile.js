7875f7355df68d91b7d9555759a15bfd850374f4
NoobSaiyan <ms19961998@gmail.com>
2020-10-01 21:52:46 +0530

    add experience

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -43,24 +43,6 @@ const EditProfile = ({
       setFormData(profileData)
     }
   }, [loading, getCurrentProfile, profile])
-  //   useEffect(() => {
-  //     getCurrentProfile()
-  //     setFormData({
-  //       company: loading || !profile.company ? '' : profile.company,
-  //       website: loading || !profile.website ? '' : profile.website,
-  //       location: loading || !profile.location ? '' : profile.location,
-  //       status: loading || !profile.status ? '' : profile.status,
-  //       skills: loading || !profile.skills ? '' : profile.skills,
-  //       githubusername:
-  //         loading || !profile.githubusername ? '' : profile.githubusername,
-  //       bio: loading || !profile.bio ? '' : profile.bio,
-  //       twitter: loading || !profile.social ? '' : profile.social.twitter,
-  //       facebook: loading || !profile.social ? '' : profile.social.facebook,
-  //       linkedin: loading || !profile.social ? '' : profile.social.linkedin,
-  //       youtube: loading || !profile.social ? '' : profile.social.youtube,
-  //       instagram: loading || !profile.social ? '' : profile.social.instagram,
-  //     })
-  //   }, [loading])
 
   const {
     company,
@@ -269,5 +251,5 @@ const mapStateToProps = (state) => ({
 })
 
 export default connect(mapStateToProps, { createProfile, getCurrentProfile })(
-  EditProfile
+  withRouter(EditProfile)
 )
