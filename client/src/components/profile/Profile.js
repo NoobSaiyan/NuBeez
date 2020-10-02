3c62b9890a2f7d5690f439dea1dbcf76e176707f
NoobSaiyan <ms19961998@gmail.com>
2020-10-02 19:49:55 +0530

    cleanup

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,9 @@ import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
+import ProfileEducation from './ProfileEducation'
+import ProfileExperience from './ProfileExperience'
+import ProfileGithub from './ProfileGithub'
 import { getProfileById } from '../../actions/profile'
 
 const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
@@ -32,6 +35,38 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
           </div>
+          <div className='profile-exp bg-white p-2'>
+            <h2 className='text-primary'>Experience</h2>
+            {profile.experience.length > 0 ? (
+              <Fragment>
+                {profile.experience.map((experience) => (
+                  <ProfileExperience
+                    key={experience._id}
+                    experience={experience}
+                  />
+                ))}
+              </Fragment>
+            ) : (
+              <h4>No experience credentials</h4>
+            )}
+          </div>
+
+          <div className='profile-edu bg-white p-2'>
+            <h2 className='text-primary'>Education</h2>
+            {profile.education.length > 0 ? (
+              <Fragment>
+                {profile.education.map((education) => (
+                  <ProfileEducation key={education._id} education={education} />
+                ))}
+              </Fragment>
+            ) : (
+              <h4>No education credentials</h4>
+            )}
+          </div>
+
+          {profile.githubusername && (
+            <ProfileGithub username={profile.githubusername} />
+          )}
         </Fragment>
       )}
     </Fragment>
