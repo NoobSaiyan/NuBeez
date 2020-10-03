55de058f554add4048c2079714c44e74a09c4204
NoobSaiyan <ms19961998@gmail.com>
2020-10-03 20:35:41 +0530

    comment form

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import PostItem from '../posts/PostItem'
-// import CommentForm from '../post/CommentForm';
+import CommentForm from '../post/CommentForm'
 // import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/post'
 
@@ -21,6 +21,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back To Posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
     </Fragment>
   )
 }
