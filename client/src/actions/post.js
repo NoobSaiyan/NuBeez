5557bc2ae1edd5f5091e4bd560b5033c1644ac72
NoobSaiyan <ms19961998@gmail.com>
2020-10-03 20:20:21 +0530

    single post display

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -3,6 +3,7 @@ import { setAlert } from './alert'
 import {
   ADD_POST,
   DELETE_POST,
+  GET_POST,
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
@@ -60,7 +61,7 @@ export const removeLike = (id) => async (dispatch) => {
 //delete post
 export const deletePost = (id) => async (dispatch) => {
   try {
-    const res = await axios.delete(`/api/posts/${id}`)
+    await axios.delete(`/api/posts/${id}`)
 
     dispatch({
       type: DELETE_POST,
@@ -85,6 +86,23 @@ export const addPost = (formData) => async (dispatch) => {
     })
 
     dispatch(setAlert('Post Created', 'success'))
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+//get post
+export const getPost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`)
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data,
+    })
   } catch (err) {
     dispatch({
       type: POST_ERROR,
